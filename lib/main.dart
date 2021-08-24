3b7e42a34b30eb8e1c6177869f30ab8b4647e9b1
Vaishnav-OM <vaishnavvom@gmail.com>
2021-08-24 19:14:40 +0530

    Attendance verification

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: VerifyAttendanceScreen(),
+      home: LoginScreen(),
     );
   }
 }
