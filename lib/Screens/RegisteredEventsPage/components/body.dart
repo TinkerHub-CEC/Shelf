a47fc80b700b1754f86cea26fa579f2243d90ee7
Akshay Pradeep <akshaypradeep14000@gmail.com>
2021-08-31 13:06:28 +0530

    Revert "Updates"
    This reverts commit eaf1aad2b7b37cf3b4d8e42e33176b637f8b4792.

--- lib/Screens/RegisteredEventsPage/components/body.dart
+++ lib/Screens/RegisteredEventsPage/components/body.dart
@@ -1,4 +1,3 @@
-import 'package:shelf/Screens/Home/components/eventcard.dart';
 import 'package:shelf/Screens/Login/login_screen.dart';
 import 'package:shelf/Screens/Signup/signup_screen.dart';
 import 'package:shelf/components/custom_menu_bar.dart';
@@ -10,26 +9,8 @@ class Body extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Container(
-      height: size.height,
-      width: size.width,
-      child: //Column(
-      //mainAxisAlignment: MainAxisAlignment.center,
-      //children: [
-      ListView(
-        physics: ScrollPhysics(),
-        scrollDirection: Axis.vertical,
-        children: <Widget>[
-          SizedBox(height: size.height * 0.04),
-          CustomMenuBar(text: "Registered Events"),
-          SizedBox(height: size.height * 0.04),
-          SizedBox(height: size.height * 0.02),
-          EventCard(),
-          SizedBox(height: size.height * 0.04),
-
-        ],
-      ),
-
+    return Scaffold(
+      body: CustomMenuBar(text: "Events"),
     );
   }
 }
