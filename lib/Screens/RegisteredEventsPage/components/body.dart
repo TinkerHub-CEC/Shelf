694aed532cb8161ed4d2fc6809d5ba7c641f7a8b
Akshay Pradeep <akshaypradeep14000@gmail.com>
2021-08-31 13:11:45 +0530

    Update

--- lib/Screens/RegisteredEventsPage/components/body.dart
+++ lib/Screens/RegisteredEventsPage/components/body.dart
@@ -1,3 +1,9 @@
+<<<<<<< HEAD
+=======
+import 'package:shelf/Screens/Home/components/eventcard.dart';
+import 'package:shelf/Screens/Login/login_screen.dart';
+import 'package:shelf/Screens/Signup/signup_screen.dart';
+>>>>>>> parent of a47fc80 (Revert "Updates")
 import 'package:shelf/components/custom_menu_bar.dart';
 import 'package:flutter/material.dart';
 
@@ -6,8 +12,26 @@ class Body extends StatelessWidget {
   Widget build(BuildContext context) {
     // ignore: unused_local_variable
     Size size = MediaQuery.of(context).size;
-    return Scaffold(
-      body: CustomMenuBar(text: "Events"),
+    return Container(
+      height: size.height,
+      width: size.width,
+      child: //Column(
+      //mainAxisAlignment: MainAxisAlignment.center,
+      //children: [
+      ListView(
+        physics: ScrollPhysics(),
+        scrollDirection: Axis.vertical,
+        children: <Widget>[
+          SizedBox(height: size.height * 0.04),
+          CustomMenuBar(text: "Registered Events"),
+          SizedBox(height: size.height * 0.04),
+          SizedBox(height: size.height * 0.02),
+          EventCard(),
+          SizedBox(height: size.height * 0.04),
+
+        ],
+      ),
+
     );
   }
 }
