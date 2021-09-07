aca65c8d6597faadd5ddcc4437c01ccb05a2c3d7
Vaishnav-OM <vaishnavvom@gmail.com>
2021-09-07 22:11:57 +0530

    Submit Attendance

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         primaryColor: kPrimaryColor,
         scaffoldBackgroundColor: Colors.white,
       ),
-      home: submitAttendance(),
+      home: LoginScreen(),
     );
   }
 }
